2b36deae58581f9890352dda3d454c6cba6b6b78
Duncan Davenport (OUTLOOK) <duncadav@microsoft.com>
2021-10-11 14:15:55 -0700

    Add packages.json

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const http = require("http");
-const fs = require('fs').promises;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/home.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
-};
+app.get('/', (req, res) => {
+    res.sendFile('home.html');
+})
 
-const host = 'localhost';
-const port = 8000;
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+// const requestListener = function (req, res) {
+//     fs.readFile(__dirname + "/home.html")
+//         .then(contents => {
+//             res.setHeader("Content-Type", "text/html");
+//             res.writeHead(200);
+//             res.end(contents);
+//         })
+//         .catch(err => {
+//             res.writeHead(500);
+//             res.end(err);
+//             return;
+//         });
+// };
+
+// const host = 'localhost';
+// const port = 8000;
+// const server = http.createServer(requestListener);
+// server.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}`);
+// });
